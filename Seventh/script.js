bdd4939fa63bc23ef70c52d9a5055b1f09353569
ocunningluv <78517275+ocunningluv@users.noreply.github.com>
2021-07-03 21:00:32 +0900

    Update script.js

--- Seventh/script.js
+++ Seventh/script.js
@@ -1,69 +1,64 @@
-/******************
-Code by Vamoss
-Original code link:
-https://www.openprocessing.org/sketch/948590
+const nbEllipses = 10;
+const smallestDiameter = 100;
+const diameterIncrement = 30;
+const controlPointOffset = 20;
 
-Author links:
-http://vamoss.com.br
-http://twitter.com/vamoss
-http://github.com/vamoss
-******************/
-
-//Original artwork "Gravité centrale" from Milan Dobeš (1929)
-//Source: milandobes.com
-
-let size, tile, grid;
-let col1, col2, col3;
-const border = 30;
+var x = 0,
+    y = 0;
+var positionHistory = [];
+var angleMeter;
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-    size = min(width-border*2, height-border*2) / 4;
-
-    col1 = color(255, 255, 255);
-    col2 = color(0);
-    col3 = color(255);
-
-    tile = createGraphics(size, size);
-    tile.noStroke();
-
-    tile.fill(col2);
-    tile.beginShape();
-        for(let a = HALF_PI; a <= PI+HALF_PI; a+=TWO_PI/30){
-            let x = sin(a) * size/2.01 + size/2;
-            let y = cos(a) * size/2.01 + size/2;
-            tile.vertex(x, y);
-        }
-    tile.endShape();
-
-    tile.fill(col3);
-    tile.beginShape();
-        for(let a = -HALF_PI; a <= HALF_PI; a+=TWO_PI/30){
-            let x = sin(a) * size/2.01 + size/2;
-            let y = cos(a) * size/2.01 + size/2;
-            tile.vertex(x, y);
-        }
-    tile.endShape();
-
-    grid = [];
-    let w = width - border * 2;
-    let h = height - border * 2;
-    let alignX = (w - floor(w/size) * size) / 2;
-    let alignY = (h - floor(h/size) * size) / 2;
-    for(let y = size/2 + alignY + border; y < h; y += size){
-        for(let x = size/2 + alignX + border; x < w; x += size){
-            grid.push({x, y});
-        }
-    }
+  createCanvas(windowWidth, windowHeight);
+  noStroke();
+  smooth();
+  while(positionHistory.length<nbEllipses*2){
+    positionHistory.push([0,0,0]);
+  }
+  angleMeter = new AngleMeter(smallestDiameter/2.0);
+  x = positionHistory[0][0];
+  y = positionHistory[0][1];
 }
 
 function draw() {
-    background(col1);
-    grid.forEach((g, index) => {
-        push();
-            translate(g.x, g.y);
-            rotate(atan2(g.y - mouseY, g.x - mouseX));
-            image(tile, - size / 2, - size / 2);
-        pop();
-    });
+  positionHistory.pop();
+  x = 0.6 * x + 0.4 * mouseX;
+  y = 0.6 * y + 0.4 * mouseY;
+  angleMeter.drag(x,y);
+  positionHistory.unshift([x,y,angleMeter.angle]);
+  drawEllipses();
+}
+
+function drawEllipses(){
+  background(255);
+  for(var i = 0; i<nbEllipses; i++){
+    let positionIndex = 2* (nbEllipses-1  - i);
+    let position = positionHistory[positionIndex];
+    let majorAxis = smallestDiameter + diameterIncrement * (nbEllipses -1 -i);
+    drawEllipse(position, majorAxis, i%2==0 ? 255:0);
+  }
+}
+
+function drawEllipse(x_y_angle, majorAxis, fillColor){
+  fill(fillColor);
+  push();
+  translate(x_y_angle[0],x_y_angle[1]);
+  rotate(x_y_angle[2]);
+  ellipse(-controlPointOffset, 0, majorAxis, majorAxis * 0.75);
+  pop();
+}
+
+class AngleMeter{
+  constructor(distance){
+    this.angle = 0;
+    this.draggedPoint = [0,0];
+    this.distance = distance;
+  }
+  drag(x, y){
+    const dx = x - this.draggedPoint[0];
+    const dy = y - this.draggedPoint[1];
+    this.angle = Math.atan2(dy, dx);
+    this.draggedPoint[0] = x - (Math.cos(this.angle) * this.distance);
+    this.draggedPoint[1] = y - (Math.sin(this.angle) * this.distance);
+  }
 }
