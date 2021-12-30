20dde402952d88e938137fed35ef70f06b96d051
verythings01 <jeong.gyeon2@gmail.com>
2021-12-30 17:26:50 +0900

    update

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
-var infospot1, infospot2, infospot3, viewer;
-const panorama = new PANOLENS.ImagePanorama("./images/8.jpg");
+var infospot1, infospot2, infospot3, infospot4, viewer;
+const panorama = new PANOLENS.ImagePanorama("./images/7-2.jpg");
 
 ////////////////////////////////////////////////
 
 infospot1 = new PANOLENS.Infospot(500, PANOLENS.DataImage.Info);
-infospot1.position.set(-1250.0, 0, -5000);
+infospot1.position.set(-1000.0, -750, -4000);
 infospot1.addHoverElement(
   document.getElementById("desc-container-1"),
   200
 );
 
 infospot2 = new PANOLENS.Infospot(500, PANOLENS.DataImage.Info);
-infospot2.position.set(7000.0, -665.23, -3996.49);
+infospot2.position.set(2500.0, -200, -3996.49);
 infospot2.addHoverElement(
   document.getElementById("desc-container-2"),
   200
@@ -24,11 +24,19 @@ infospot3.addHoverElement(
   200
 );
 
+infospot4 = new PANOLENS.Infospot(500, PANOLENS.DataImage.Info);
+infospot4.position.set(500.0, -750.25, 197.56);
+infospot4.addHoverElement(
+  document.getElementById("desc-container-4"),
+  200
+);
+
 ////////////////////////////////////////////
 
 panorama.add(infospot1);
 panorama.add(infospot2);
 panorama.add(infospot3);
+panorama.add(infospot4);
 
 viewer = new PANOLENS.Viewer();
 viewer.add(panorama);
